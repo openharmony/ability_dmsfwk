0ed0b700262971778f26971ccd25f0c4bd01bff5
MisterE <smart_e@126.com>
2025-03-22 15:48:39 +0800

    适配pc更换主板，保留存储，导致udid变化的场景
    Signed-off-by: MisterE <smart_e@126.com>

--- services/dtbschedmgr/include/mission/distributed_bm_storage.h
+++ services/dtbschedmgr/include/mission/distributed_bm_storage.h
@@ -88,6 +88,8 @@ private:
     bool DealGetBundleName(const std::string &networkId, const uint16_t& bundleNameId, std::string &bundleName);
     uint16_t CreateBundleNameId(const std::string &bundleName, bool isPackageChange = false);
     void AddBundleNameId(const uint16_t &bundleNameId, const std::string &bundleName);
+    void CleanRedundancyBundleInfo(const std::string &localUdid, AppExecFwk::BundleInfo &bundleInfo,
+        const std::string &keyOfKVStore, const std::string &oldUdid) const;
     void SyncBundleInfoData(std::vector<DistributedKv::Entry> &entries);
     void DelBundleNameId(const std::string &bundleName);
     DmsBundleInfo ConvertToDistributedBundleInfo(const AppExecFwk::BundleInfo &bundleInfo,
