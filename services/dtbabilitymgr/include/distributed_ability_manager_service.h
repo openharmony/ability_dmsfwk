dd77b83aaf18c1ed2e08dc3b0d2a5e22316bff0c
BrainL <libuyan1@huawei.com>
2025-06-03 14:03:47 +0800

    fixed codecheck.
    Signed-off-by: BrainL <libuyan1@huawei.com>
    Change-Id: I08167fa56ead35fb1d4887244b3f5bb403fd25f9

--- services/dtbabilitymgr/include/distributed_ability_manager_service.h
+++ services/dtbabilitymgr/include/distributed_ability_manager_service.h
@@ -13,88 +13,87 @@
  * limitations under the License.
  */
 
- #ifndef OHOS_DISTRIBUTED_ABILITY_MANAGER_SERVICE_H
- #define OHOS_DISTRIBUTED_ABILITY_MANAGER_SERVICE_H
- 
- #include "bundlemgr/bundle_mgr_interface.h"
- #include "bundlemgr/bundle_mgr_proxy.h"
- #include "continuation_manager/notifier_info.h"
- #include "distributed_ability_manager_stub.h"
- #include "event_handler.h"
- #include "ffrt.h"
- #include "single_instance.h"
- #include "system_ability.h"
- 
- namespace OHOS {
- namespace DistributedSchedule {
- class DistributedAbilityManagerService : public SystemAbility, public DistributedAbilityManagerStub {
-     DECLARE_SYSTEM_ABILITY(DistributedAbilityManagerService);
-     DECLARE_SINGLE_INSTANCE_BASE(DistributedAbilityManagerService);
- 
- public:
-     ~DistributedAbilityManagerService() = default;
-     void OnStart() override;
-     void OnStop() override;
-     int32_t Dump(int32_t fd, const std::vector<std::u16string>& args) override;
- 
-     int32_t Register(
-         const std::shared_ptr<ContinuationExtraParams>& continuationExtraParams, int32_t& token) override;
-     int32_t Unregister(int32_t token) override;
-     int32_t RegisterDeviceSelectionCallback(
-         int32_t token, const std::string& cbType, const sptr<IRemoteObject>& notifier) override;
-     int32_t UnregisterDeviceSelectionCallback(int32_t token, const std::string& cbType) override;
-     int32_t UpdateConnectStatus(int32_t token, const std::string& deviceId,
-         DeviceConnectStatus deviceConnectStatus) override;
-     int32_t StartDeviceManager(
-         int32_t token, const std::shared_ptr<ContinuationExtraParams>& continuationExtraParams = nullptr) override;
-     int32_t RegisterWithoutExtraParam(int32_t& token) override;
-     int32_t StartDeviceManagerWithoutExtraParam(int32_t token) override;
- 
-     void ProcessNotifierDied(const sptr<IRemoteObject>& notifier);
-     void ScheduleStartDeviceManager(const sptr<IRemoteObject>& appProxy, int32_t token,
-         const std::shared_ptr<ContinuationExtraParams>& continuationExtraParams = nullptr);
-     int32_t OnDeviceConnect(int32_t token, const std::vector<ContinuationResult>& continuationResults);
-     int32_t OnDeviceDisconnect(int32_t token, const std::vector<ContinuationResult>& continuationResults);
-     int32_t OnDeviceCancel();
-     void DumpAppRegisterInfo(std::string& info);
- private:
-     DistributedAbilityManagerService();
-     bool IsExceededRegisterMaxNum(uint32_t accessToken);
-     bool IsContinuationModeValid(ContinuationMode continuationMode);
-     bool IsConnectStatusValid(DeviceConnectStatus deviceConnectStatus);
-     bool IsTokenRegistered(uint32_t accessToken, int32_t token);
-     bool IsNotifierRegistered(int32_t token);
-     bool IsNotifierRegisteredLocked(int32_t token, const std::string& cbType);
-     bool QueryTokenByNotifier(const sptr<IRemoteObject>& notifier, int32_t& token);
-     bool HandleDeviceConnect(const sptr<IRemoteObject>& notifier,
-         const std::vector<ContinuationResult>& continuationResults);
-     bool HandleDeviceDisconnect(const sptr<IRemoteObject>& notifier,
-         const std::vector<ContinuationResult>& continuationResults);
-     int32_t ConnectAbility(int32_t token, const std::shared_ptr<ContinuationExtraParams>& continuationExtraParams);
-     int32_t DisconnectAbility();
-     bool QueryExtensionAbilityInfo(const int32_t& activeAccountId, const AAFwk::Want& want,
-         AppExecFwk::ExtensionAbilityInfo& extensionInfo);
-     bool HandleDisconnectAbility();
-     void HandleNotifierDied(const sptr<IRemoteObject>& notifier);
-     void HandleStartDeviceManager(int32_t token,
-         const std::shared_ptr<ContinuationExtraParams>& continuationExtraParams = nullptr);
-     void HandleUpdateConnectStatus(int32_t token, std::string deviceId,
-         const DeviceConnectStatus& deviceConnectStatus);
-     void DumpNotifierLocked(const std::vector<int32_t>& tokenVec, std::string& info);
- 
-     std::mutex tokenMutex_;
-     std::atomic<int32_t> token_ {0};
-     std::mutex tokenMapMutex_;
-     std::map<uint32_t, std::vector<int32_t>> tokenMap_;
-     std::mutex callbackMapMutex_;
-     std::map<int32_t, std::unique_ptr<NotifierInfo>> callbackMap_;
-     sptr<IRemoteObject::DeathRecipient> notifierDeathRecipient_;
-     sptr<IRemoteObject> connect_;
-     std::mutex appProxyMutex_;
-     sptr<IRemoteObject> appProxy_;
-     std::shared_ptr<ffrt::queue> continuationHandler_;
- };
- } // namespace DistributedSchedule
- } // namespace OHOS
- #endif // OHOS_DISTRIBUTED_ABILITY_MANAGER_SERVICE_H
- 
+#ifndef OHOS_DISTRIBUTED_ABILITY_MANAGER_SERVICE_H
+#define OHOS_DISTRIBUTED_ABILITY_MANAGER_SERVICE_H
+
+#include "bundlemgr/bundle_mgr_interface.h"
+#include "bundlemgr/bundle_mgr_proxy.h"
+#include "continuation_manager/notifier_info.h"
+#include "distributed_ability_manager_stub.h"
+#include "event_handler.h"
+#include "ffrt.h"
+#include "single_instance.h"
+#include "system_ability.h"
+
+namespace OHOS {
+namespace DistributedSchedule {
+class DistributedAbilityManagerService : public SystemAbility, public DistributedAbilityManagerStub {
+    DECLARE_SYSTEM_ABILITY(DistributedAbilityManagerService);
+    DECLARE_SINGLE_INSTANCE_BASE(DistributedAbilityManagerService);
+
+public:
+    ~DistributedAbilityManagerService() = default;
+    void OnStart() override;
+    void OnStop() override;
+    int32_t Dump(int32_t fd, const std::vector<std::u16string>& args) override;
+
+    int32_t Register(
+        const std::shared_ptr<ContinuationExtraParams>& continuationExtraParams, int32_t& token) override;
+    int32_t Unregister(int32_t token) override;
+    int32_t RegisterDeviceSelectionCallback(
+        int32_t token, const std::string& cbType, const sptr<IRemoteObject>& notifier) override;
+    int32_t UnregisterDeviceSelectionCallback(int32_t token, const std::string& cbType) override;
+    int32_t UpdateConnectStatus(int32_t token, const std::string& deviceId,
+        DeviceConnectStatus deviceConnectStatus) override;
+    int32_t StartDeviceManager(
+        int32_t token, const std::shared_ptr<ContinuationExtraParams>& continuationExtraParams = nullptr) override;
+    int32_t RegisterWithoutExtraParam(int32_t& token) override;
+    int32_t StartDeviceManagerWithoutExtraParam(int32_t token) override;
+
+    void ProcessNotifierDied(const sptr<IRemoteObject>& notifier);
+    void ScheduleStartDeviceManager(const sptr<IRemoteObject>& appProxy, int32_t token,
+        const std::shared_ptr<ContinuationExtraParams>& continuationExtraParams = nullptr);
+    int32_t OnDeviceConnect(int32_t token, const std::vector<ContinuationResult>& continuationResults);
+    int32_t OnDeviceDisconnect(int32_t token, const std::vector<ContinuationResult>& continuationResults);
+    int32_t OnDeviceCancel();
+    void DumpAppRegisterInfo(std::string& info);
+private:
+    DistributedAbilityManagerService();
+    bool IsExceededRegisterMaxNum(uint32_t accessToken);
+    bool IsContinuationModeValid(ContinuationMode continuationMode);
+    bool IsConnectStatusValid(DeviceConnectStatus deviceConnectStatus);
+    bool IsTokenRegistered(uint32_t accessToken, int32_t token);
+    bool IsNotifierRegistered(int32_t token);
+    bool IsNotifierRegisteredLocked(int32_t token, const std::string& cbType);
+    bool QueryTokenByNotifier(const sptr<IRemoteObject>& notifier, int32_t& token);
+    bool HandleDeviceConnect(const sptr<IRemoteObject>& notifier,
+        const std::vector<ContinuationResult>& continuationResults);
+    bool HandleDeviceDisconnect(const sptr<IRemoteObject>& notifier,
+        const std::vector<ContinuationResult>& continuationResults);
+    int32_t ConnectAbility(int32_t token, const std::shared_ptr<ContinuationExtraParams>& continuationExtraParams);
+    int32_t DisconnectAbility();
+    bool QueryExtensionAbilityInfo(const int32_t& activeAccountId, const AAFwk::Want& want,
+        AppExecFwk::ExtensionAbilityInfo& extensionInfo);
+    bool HandleDisconnectAbility();
+    void HandleNotifierDied(const sptr<IRemoteObject>& notifier);
+    void HandleStartDeviceManager(int32_t token,
+        const std::shared_ptr<ContinuationExtraParams>& continuationExtraParams = nullptr);
+    void HandleUpdateConnectStatus(int32_t token, std::string deviceId,
+        const DeviceConnectStatus& deviceConnectStatus);
+    void DumpNotifierLocked(const std::vector<int32_t>& tokenVec, std::string& info);
+
+    std::mutex tokenMutex_;
+    std::atomic<int32_t> token_ {0};
+    std::mutex tokenMapMutex_;
+    std::map<uint32_t, std::vector<int32_t>> tokenMap_;
+    std::mutex callbackMapMutex_;
+    std::map<int32_t, std::unique_ptr<NotifierInfo>> callbackMap_;
+    sptr<IRemoteObject::DeathRecipient> notifierDeathRecipient_;
+    sptr<IRemoteObject> connect_;
+    std::mutex appProxyMutex_;
+    sptr<IRemoteObject> appProxy_;
+    std::shared_ptr<ffrt::queue> continuationHandler_;
+};
+} // namespace DistributedSchedule
+} // namespace OHOS
+#endif // OHOS_DISTRIBUTED_ABILITY_MANAGER_SERVICE_H
