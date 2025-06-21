ac9b1af739ceb0158496226cd4c3247ae862fe3c
韦国庆 <weiguoqing2@huawei.com>
2025-06-21 19:05:37 +0800

    add FUZZ
    Signed-off-by: 韦国庆 <weiguoqing2@huawei.com>

--- test/fuzztest/distributedwantparams_fuzzer/distributedwantparams_fuzzer.cpp
+++ test/fuzztest/distributedwantparams_fuzzer/distributedwantparams_fuzzer.cpp
@@ -47,6 +47,11 @@ constexpr int32_t POS_3 = 3;
 constexpr int32_t OFFSET_24 = 24;
 constexpr int32_t OFFSET_16 = 16;
 constexpr int32_t OFFSET_8 = 8;
+
+inline size_t CalculateMid(size_t size)
+{
+    return size / 2;
+}
 }
 uint32_t GetU32Data(const char* ptr)
 {
@@ -201,6 +206,100 @@ bool DoSomethingInterestingWithMyApiDistributedWantParams004(const uint8_t* data
     wantParams->ToWantParams();
     return true;
 }
+
+bool DistributedWantParamWrapperEqualsFuzzTest(const uint8_t* data, size_t size)
+{
+    if (data == nullptr || size == 0) {
+        return false;
+    }
+    size_t mid = CalculateMid(size);
+
+    DistributedWantParams wantOther;
+    std::shared_ptr<DistributedWantParams> wantParams1 = std::make_shared<DistributedWantParams>(wantOther);
+    std::string key1(reinterpret_cast<const char*>(data), mid);
+    std::string value1(reinterpret_cast<const char*>(data + mid), size - mid);
+    wantParams1->SetParam(key1, String::Box(value1));
+
+    std::shared_ptr<DistributedWantParams> wantParams2 = std::make_shared<DistributedWantParams>(wantOther);
+    std::string key2(reinterpret_cast<const char*>(data + mid), size - mid);
+    std::string value2(reinterpret_cast<const char*>(data), mid);
+    wantParams2->SetParam(key2, String::Box(value2));
+
+    DistributedWantParamWrapper wrapper1(*wantParams1);
+    DistributedWantParamWrapper wrapper2(*wantParams2);
+
+    bool result = wrapper1.Equals(wrapper2);
+
+    return result;
+}
+
+bool DistributedWantParamWrapperFindMatchingBraceFuzzTest(const uint8_t* data, size_t size)
+{
+    if (data == nullptr || size == 0) {
+        return false;
+    }
+    std::string inputStr(reinterpret_cast<const char*>(data), size);
+
+    size_t startPos = size > 0 ? data[0] % size : 0;
+
+    DistributedWantParams wantOther;
+    std::shared_ptr<DistributedWantParams> wantParams = std::make_shared<DistributedWantParams>(wantOther);
+    DistributedWantParamWrapper wrapper(*wantParams);
+    size_t result = wrapper.FindMatchingBrace(inputStr, startPos);
+
+    return result >= startPos && result <= inputStr.size();
+}
+
+bool DistributedWantParamWrapperBoxFuzzTest(const uint8_t* data, size_t size)
+{
+    if (data == nullptr || size == 0) {
+        return false;
+    }
+    size_t mid = CalculateMid(size);
+    std::string key(reinterpret_cast<const char*>(data), mid);
+    std::string value(reinterpret_cast<const char*>(data + mid), size - mid);
+
+    DistributedWantParams wantParams;
+    wantParams.SetParam(key, String::Box(value));
+    auto boxedObject = DistributedWantParamWrapper::Box(std::move(wantParams));
+
+    if (boxedObject == nullptr) {
+        return false;
+    }
+
+    return true;
+}
+
+bool DistributedWantParamWrapperGerTypedIdFuzzTest(const uint8_t* data, size_t size)
+{
+    if (data == nullptr || size == 0) {
+        return false;
+    }
+    std::string inputStr(reinterpret_cast<const char*>(data), size);
+
+    size_t startPos = size > 0 ? data[0] % size : 0;
+
+    DistributedWantParams wantOther;
+    std::shared_ptr<DistributedWantParams> wantParams = std::make_shared<DistributedWantParams>(wantOther);
+    DistributedWantParamWrapper wrapper(*wantParams);
+    int typeId = wrapper.GerTypedId(inputStr, startPos);
+
+    return typeId >= 0;
+}
+bool DistributedWantParamWrapperParseWantParamsFuzzTest(const uint8_t* data, size_t size)
+{
+    if (data == nullptr || size == 0) {
+        return false;
+    }
+    std::string inputStr(reinterpret_cast<const char*>(data), size);
+
+    DistributedWantParams wantOther;
+    std::shared_ptr<DistributedWantParams> wantParams = std::make_shared<DistributedWantParams>(wantOther);
+    DistributedWantParamWrapper wrapper(*wantParams);
+    DistributedWantParams result = wrapper.ParseWantParams(inputStr);
+
+    return result.Size() >= 0;
+}
 }
 
 /* Fuzzer entry point */
@@ -210,5 +309,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
     OHOS::DoSomethingInterestingWithMyApiDistributedWantParams002(data, size);
     OHOS::DoSomethingInterestingWithMyApiDistributedWant003(data, size);
     OHOS::DoSomethingInterestingWithMyApiDistributedWantParams004(data, size);
+    OHOS::DistributedWantParamWrapperEqualsFuzzTest(data, size);
+    OHOS::DistributedWantParamWrapperFindMatchingBraceFuzzTest(data, size);
+    OHOS::DistributedWantParamWrapperBoxFuzzTest(data, size);
+    OHOS::DistributedWantParamWrapperGerTypedIdFuzzTest(data, size);
+    OHOS::DistributedWantParamWrapperParseWantParamsFuzzTest(data, size);
     return 0;
 }
