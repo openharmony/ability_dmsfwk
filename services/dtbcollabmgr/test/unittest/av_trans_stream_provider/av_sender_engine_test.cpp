c317d839787325f63a1db9e526221975cbceb6bc
xuerunlong <xuerunlong4@h-partners.com>
2025-12-29 11:43:03 +0800

    fix tdd err
    Signed-off-by: xuerunlong <xuerunlong4@h-partners.com>

--- services/dtbcollabmgr/test/unittest/av_trans_stream_provider/av_sender_engine_test.cpp
+++ services/dtbcollabmgr/test/unittest/av_trans_stream_provider/av_sender_engine_test.cpp
@@ -371,7 +371,7 @@ HWTEST_F(AVSenderEngineTest, Start_Test, TestSize.Level1)
         "builtin.dtbcollab.sender", FilterType::FILTERTYPE_SOURCE);
     senderEngine_->curState_ = EngineState::START;
     ret = senderEngine_->Start();
-    EXPECT_EQ(ret, static_cast<int32_t>(Status::OK));
+    EXPECT_EQ(ret, static_cast<int32_t>(Status::ERROR_WRONG_STATE));
 
     senderEngine_->curState_ = EngineState::SETTING;
     ret = senderEngine_->Start();
@@ -397,11 +397,11 @@ HWTEST_F(AVSenderEngineTest, Stop_Test, TestSize.Level1)
     DTEST_LOG << "AVSenderEngineTest Prepare_Test begin" << std::endl;
     senderEngine_->curState_ = EngineState::INIT;
     auto ret = senderEngine_->Stop();
-    EXPECT_EQ(ret, static_cast<int32_t>(Status::OK));
+    EXPECT_EQ(ret, static_cast<int32_t>(Status::ERROR_WRONG_STATE));
 
     senderEngine_->curState_ = EngineState::STOP;
     ret = senderEngine_->Stop();
-    EXPECT_EQ(ret, static_cast<int32_t>(Status::OK));
+    EXPECT_EQ(ret, static_cast<int32_t>(Status::ERROR_WRONG_STATE));
 
     senderEngine_->curState_ = EngineState::SETTING;
     ret = senderEngine_->Stop();
