5de47402cedb43518bcd9bb7573630cead558367
wei-guoqing1 <weiguoqing2@h-partners.com>
2026-03-12 11:01:10 +0800

    fuzz 修复
    Signed-off-by: wei-guoqing1 <weiguoqing2@h-partners.com>

---
+++ test/fuzztest/dschedassemble_fuzzer/dschedassemble_fuzzer.cpp
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "dschedassemble_fuzzer.h"
+
+#include <cstddef>
+#include <cstdint>
+#include <fuzzer/FuzzedDataProvider.h>
+#include <securec.h>
+
+#include "dsched_data_buffer.h"
+#include "dsched_softbus_session.h"
+
+namespace OHOS {
+namespace DistributedSchedule {
+namespace {
+constexpr size_t MIN_FRAG_BUFFER_SIZE = 16;
+constexpr size_t MIN_NO_FRAG_BUFFER_SIZE = 8;
+constexpr size_t MIN_NO_FRAG_BUF_SIZE = 8;
+constexpr size_t MAX_NO_FRAG_BUF_SIZE = 256;
+constexpr size_t MIN_FRAG_BUF_SIZE = 16;
+constexpr size_t MAX_FRAG_BUF_SIZE = 512;
+constexpr uint8_t MIN_FRAG_FLAG = 0;
+constexpr uint8_t MAX_FRAG_FLAG = 3;
+constexpr uint32_t MIN_DATA_LEN = 0;
+constexpr int32_t TOTAL_LEN_MULTIPLIER = 2;
+}
+
+void AssembleNoFragFuzzTest(const uint8_t* data, size_t size)
+{
+    if (!data || size < MIN_NO_FRAG_BUFFER_SIZE) {
+        return;
+    }
+    FuzzedDataProvider fdp(data, size);
+
+    size_t bufSize = fdp.ConsumeIntegralInRange<size_t>(MIN_NO_FRAG_BUF_SIZE, MAX_NO_FRAG_BUF_SIZE);
+    std::shared_ptr<DSchedDataBuffer> buffer = std::make_shared<DSchedDataBuffer>(bufSize);
+    std::vector<uint8_t> bufContent = fdp.ConsumeBytes<uint8_t>(bufSize);
+    if (memcpy_s(buffer->Data(), bufSize, bufContent.data(), bufContent.size()) != EOK) {
+        return;
+    }
+
+    DSchedSoftbusSession::SessionDataHeader headerPara;
+    headerPara.dataLen = fdp.ConsumeIntegralInRange<uint32_t>(MIN_DATA_LEN, bufSize);
+    headerPara.totalLen = headerPara.dataLen;
+    headerPara.dataType = fdp.ConsumeIntegral<uint32_t>();
+
+    DSchedSoftbusSession session;
+    session.AssembleNoFrag(buffer, headerPara);
+}
+
+void AssembleFragFuzzTest(const uint8_t* data, size_t size)
+{
+    if (!data || size < MIN_FRAG_BUFFER_SIZE) {
+        return;
+    }
+    FuzzedDataProvider fdp(data, size);
+
+    size_t bufSize = fdp.ConsumeIntegralInRange<size_t>(MIN_FRAG_BUF_SIZE, MAX_FRAG_BUF_SIZE);
+    std::shared_ptr<DSchedDataBuffer> buffer = std::make_shared<DSchedDataBuffer>(bufSize);
+    std::vector<uint8_t> bufContent = fdp.ConsumeBytes<uint8_t>(bufSize);
+    if (memcpy_s(buffer->Data(), bufSize, bufContent.data(), bufContent.size()) != EOK) {
+        return;
+    }
+    DSchedSoftbusSession::SessionDataHeader headerPara;
+    headerPara.fragFlag = fdp.ConsumeIntegralInRange<uint8_t>(MIN_FRAG_FLAG, MAX_FRAG_FLAG);
+    headerPara.seqNum = fdp.ConsumeIntegral<uint32_t>();
+    headerPara.subSeq = fdp.ConsumeIntegral<uint16_t>();
+    headerPara.dataLen = fdp.ConsumeIntegralInRange<uint32_t>(MIN_DATA_LEN, bufSize);
+    headerPara.totalLen = fdp.ConsumeIntegralInRange<uint32_t>(headerPara.dataLen, bufSize * TOTAL_LEN_MULTIPLIER);
+    headerPara.dataType = fdp.ConsumeIntegral<uint32_t>();
+
+    DSchedSoftbusSession session;
+    session.AssembleFrag(buffer, headerPara);
+}
+}
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    OHOS::DistributedSchedule::AssembleNoFragFuzzTest(data, size);
+    OHOS::DistributedSchedule::AssembleFragFuzzTest(data, size);
+    return 0;
+}
