257b523cbeca46e76d8f35cd464a0706b7cbeef1
MisterE <huangfuyi2@h-partners.com>
2026-08-14 09:10:06 +0800

    安全加固
    Signed-off-by: MisterE<smart_e@126.com>
    Co-Authored-By: Agent

--- services/dtbschedmgr/include/softbus_adapter/transport/dsched_transport_softbus_adapter.h
+++ services/dtbschedmgr/include/softbus_adapter/transport/dsched_transport_softbus_adapter.h
@@ -35,7 +35,6 @@ constexpr int32_t DSCHED_QOS_TYPE_MIN_BW = 80 * 1024;
 constexpr int32_t DSCHED_COLLAB_LOW_QOS_TYPE_MIN_BW = 40 * 1024 * 1024;
 constexpr int32_t DSCHED_QOS_TYPE_MAX_LATENCY = 6000;
 constexpr int32_t DSCHED_QOS_TYPE_MIN_LATENCY = 1000;
-
 constexpr const char* SOCKET_DMS_SESSION_NAME = "ohos.distributedschedule.dms.connect";
 constexpr const char* SOCKET_DMS_PKG_NAME = "dms";
 constexpr const char* SOCKET_DMS_INTENT_NAME = "ohos.distributedschedule.dms.intent";
